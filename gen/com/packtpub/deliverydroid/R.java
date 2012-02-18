3181372c5848c56f83754931de329c66c31d10a7
KyoungRok Jang <astray0924@gmail.com>
2012-02-18 16:34:26 +0900

    Before "Have a go hero"

--- gen/com/packtpub/deliverydroid/R.java
+++ gen/com/packtpub/deliverydroid/R.java
@@ -9,20 +9,28 @@ package com.packtpub.deliverydroid;
 
 public final class R {
     public static final class array {
-        public static final int restaurants=0x7f050000;
+        public static final int restaurants=0x7f060000;
     }
     public static final class attr {
+    }
+    public static final class dimen {
+        public static final int item_inner_height=0x7f040002;
+        public static final int item_outer_height=0x7f040000;
+        public static final int item_text_size=0x7f040003;
+        public static final int menu_item_height=0x7f040001;
+        public static final int padding=0x7f040004;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int restaurant=0x7f060000;
+        public static final int restaurant=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int menu_item=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
     }
 }
--- src/com/packtpub/deliverydroid/SelectRestaurantActivity.java
+++ src/com/packtpub/deliverydroid/SelectRestaurantActivity.java
@@ -2,6 +2,8 @@ package com.packtpub.deliverydroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SelectRestaurantActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,12 @@ public class SelectRestaurantActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final ListView restaurants = (ListView)findViewById(R.id.restaurant);
+        restaurants.setAdapter(new ArrayAdapter<String>(
+        		this,
+        		R.layout.menu_item,
+        		getResources().getStringArray(R.array.restaurants)));
+        
     }
 }
