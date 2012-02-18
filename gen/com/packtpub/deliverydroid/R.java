6ed7356c6642b3f8271581f09d3d3f392fc2eba3
KyoungRok Jang <astray0924@gmail.com>
2012-02-18 17:14:03 +0900

    until 52pg

--- gen/com/packtpub/deliverydroid/R.java
+++ gen/com/packtpub/deliverydroid/R.java
@@ -24,11 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int restaurant=0x7f070000;
+        public static final int counter=0x7f070000;
+        public static final int restaurant=0x7f070002;
+        public static final int text=0x7f070001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int menu_item=0x7f030001;
+        public static final int burger_item=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int menu_item=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
